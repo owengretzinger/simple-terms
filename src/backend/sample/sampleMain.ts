d0d544102f54e7c64a1e102a6b5e33e477322743
Virendra <vsjethra13@gmail.com>
2023-09-16 17:14:26 -0400

    design

---
+++ src/backend/sample/sampleMain.ts
@@ -0,0 +1,7 @@
+export const sampleMain = [
+    {
+    "id": 1,
+    "practice": "You should not use this service.",
+    "rating": 1
+    }
+]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {
-      extend: {
-          colors: {
-              primary: '#1B73E8',
-          },
-      },
-  },
-  plugins: [],
-};
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#1B73E8',
+            },
+        },
+        fontFamily: {
+          title: ['Exo 2', 'sans-serif'],
+        }
+    },
+    plugins: [],
+  };
